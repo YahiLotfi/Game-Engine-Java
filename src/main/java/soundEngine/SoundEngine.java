33c0bf87ff3e7510449b6208f19258d1834dcfbd
zmryasmine <76445695+zmryasmine@users.noreply.github.com>
2023-11-26 18:02:32 +0100

    sound
    on

---
+++ src/main/java/soundEngine/SoundEngine.java
@@ -0,0 +1,152 @@
+package soundEngine;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class SoundEngine {
+    Long currentFrame;
+    public Clip clip;
+
+    // current status of clip
+    public String status;
+
+    AudioInputStream audioInputStream;
+     String filePath;
+
+    public SoundEngine(){
+        this.status="new";
+    }
+
+    public void load(String filePath)
+            throws UnsupportedAudioFileException,
+            IOException, LineUnavailableException
+    {
+        // create AudioInputStream object
+        audioInputStream =
+                AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+
+        // create clip reference
+        clip = AudioSystem.getClip();
+
+        // open audioInputStream to the clip
+        clip.open(audioInputStream);
+
+        //clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void playBack()
+    {
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+        clip.start();
+        status = "playBack";
+    }
+    public void play()
+    {
+        //start the clip
+        clip.start();
+
+        status = "play";
+    }
+
+    // Method to pause the audio
+    public void pause()
+    {
+        if (status.equals("paused"))
+        {
+            System.out.println("audio is already paused");
+            return;
+        }
+        this.currentFrame =
+                this.clip.getMicrosecondPosition();
+        clip.stop();
+        status = "paused";
+    }
+
+    // Method to resume the audio
+    public void resumeAudio(String filePath) throws UnsupportedAudioFileException,
+            IOException, LineUnavailableException
+    {
+        if (status.equals("play"))
+        {
+            System.out.println("Audio is already "+
+                    "being played");
+            return;
+        }
+        clip.close();
+        resetAudioStream(filePath);
+        clip.setMicrosecondPosition(currentFrame);
+        this.play();
+    }
+
+    // Method to restart the audio
+    public void restart(String filePath) throws IOException, LineUnavailableException,
+            UnsupportedAudioFileException
+    {
+        clip.stop();
+        clip.close();
+        resetAudioStream( filePath);
+        currentFrame = 0L;
+        clip.setMicrosecondPosition(0);
+        this.play();
+    }
+
+    // Method to stop the audio
+    public void stop() throws UnsupportedAudioFileException,
+            IOException, LineUnavailableException
+    {
+        currentFrame = 0L;
+        clip.stop();
+        clip.close();
+        status = "stopped";
+    }
+
+    // Method to reset audio stream
+    public void resetAudioStream(String filePath) throws UnsupportedAudioFileException, IOException,
+            LineUnavailableException
+    {
+        audioInputStream = AudioSystem.getAudioInputStream(
+                new File(filePath).getAbsoluteFile());
+        clip.open(audioInputStream);
+        //clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public static void main(String[] args) {
+        try {
+            SoundEngine soundEngine = new SoundEngine();
+            String filePath = "src/main/resources/sample/Sounds/background1.wav";
+            soundEngine.load(filePath);
+            soundEngine.playBack();
+
+            SoundEngine soundEngine2 = new SoundEngine();
+            String filePath2 = "src/main/resources/sample/Sounds/wall.wav";
+            soundEngine2.load(filePath2);
+
+            int i = 0;
+
+            while (true) {
+                System.out.println(i);
+                i++;
+                if (i % 1000 == 0) {
+                    soundEngine2.load(filePath2);
+                    soundEngine2.play();
+                    Thread.sleep(2000);
+                }
+
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+    }
+
+
+
+}
---
+++ src/test/java/SoundEngineTest.java
@@ -0,0 +1,95 @@
+import soundEngine.SoundEngine;
+
+import org.junit.jupiter.api.Test;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SoundEngineTest {
+
+    @Test
+    void testLoad() {
+        SoundEngine soundEngine = new SoundEngine();
+        String filePath = "src/main/resources/sample/Sounds/audio4.wav";
+        try {
+            soundEngine.load(filePath);
+            assertNotNull(soundEngine.clip);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            fail("Exception thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testPlay() {
+        SoundEngine soundEngine = new SoundEngine();
+        String filePath = "src/main/resources/sample/Sounds/audio4.wav";
+        try {
+            soundEngine.load(filePath);
+            soundEngine.play();
+            assertEquals("play", soundEngine.status);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            fail("Exception thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testPause() {
+        SoundEngine soundEngine = new SoundEngine();
+        String filePath = "src/main/resources/sample/Sounds/audio4.wav";
+        try {
+            soundEngine.load(filePath);
+            soundEngine.play();
+            soundEngine.pause();
+            assertEquals("paused", soundEngine.status);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            fail("Exception thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testResumeAudio() {
+        SoundEngine soundEngine = new SoundEngine();
+        String filePath = "src/main/resources/sample/Sounds/audio4.wav";
+        try {
+            soundEngine.load(filePath);
+            soundEngine.play();
+            soundEngine.pause();
+            soundEngine.resumeAudio(filePath);
+            assertEquals("play", soundEngine.status);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            fail("Exception thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testRestart() {
+        SoundEngine soundEngine = new SoundEngine();
+        String filePath = "src/main/resources/sample/Sounds/audio4.wav";
+        try {
+            soundEngine.load(filePath);
+            soundEngine.play();
+            soundEngine.restart(filePath);
+            assertEquals("play", soundEngine.status);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            fail("Exception thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void testStop() {
+        SoundEngine soundEngine = new SoundEngine();
+        String filePath = "src/main/resources/sample/Sounds/audio4.wav";
+        try {
+            soundEngine.load(filePath);
+            soundEngine.play();
+            soundEngine.stop();
+            assertEquals("stopped", soundEngine.status);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            fail("Exception thrown: " + e.getMessage());
+        }
+    }
+
+
+}
